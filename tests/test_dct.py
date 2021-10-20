110f22e7034626c9b762636e1075b6cc87a518f3
Martin Benes <martinbenes1996@gmail.com>
2021-10-20 16:29:17 +0200

    image height width swapped

--- tests/test_dct.py
+++ tests/test_dct.py
@@ -59,7 +59,7 @@ class TestDCT(unittest.TestCase):
         img = jpeg_toolbox.load('examples/IMG_0791.jpeg')
         # process
         
-        YT = img['coef_arrays'][0].reshape((1,8,8,int(img['image_height']/8),-1))
+        YT = img['coef_arrays'][0].reshape((1,int(img['image_height']/8),-1,8,8))
         YT = np.einsum('acbde->aedbc', YT)
         CbCrT = np.stack([
             img['coef_arrays'][1].T.reshape((int(img['image_height']/8/2),-1,8,8), order='F'),
