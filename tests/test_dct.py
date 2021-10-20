987a1b6f69b5114be069e1b85e7cbd922b7060d1
Martin Benes <martinbenes1996@gmail.com>
2021-10-20 16:13:22 +0200

    axis swapping

--- tests/test_dct.py
+++ tests/test_dct.py
@@ -59,7 +59,8 @@ class TestDCT(unittest.TestCase):
         img = jpeg_toolbox.load('examples/IMG_0791.jpeg')
         # process
         
-        YT = img['coef_arrays'][0].T.reshape((1,int(img['image_width']/8),-1,64))
+        YT = img['coef_arrays'][0].T.reshape((1,8,8,int(img['image_width']/8),-1))
+        YT = np.einsum('abcde->adebc', YT)
         CbCrT = np.stack([
             img['coef_arrays'][1].T.reshape((int(img['image_width']/8/2),-1,8,8), order='F'),
             img['coef_arrays'][2].T.reshape((int(img['image_width']/8/2),-1,8,8), order='F')
