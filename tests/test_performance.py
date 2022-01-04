f0c1a473f77eb2d7c913a566c79260db8a01f951
Martin Benes <martinbenes1996@gmail.com>
2022-01-04 09:47:56 +0100

    logging of performance

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,8 +1,9 @@
 
 
+import logging
 import numpy as np
-from scipy.stats import ttest_ind
 from PIL import Image
+from scipy.stats import ttest_ind
 import sys
 import unittest
 
@@ -32,6 +33,7 @@ class TestPerformance(unittest.TestCase):
             stego.append(t_stego.stop())
         # test it isn't more than 3x slower
         max_3x_slower = ttest_ind(np.array(pil)*3, stego, alternative='less')
+        logging.info("performance %.2fs vs. %.2fs of PIL" % (np.mean(pil), np.mean(stego)))
         self.assertGreater(max_3x_slower.pvalue, .05)
     
     def test_writing(self):
