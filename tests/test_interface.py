2a8adcbc11fc760a7269d6bf029dece70363e2f0
Martin Benes <martinbenes1996@gmail.com>
2022-01-04 09:35:11 +0100

    interface test

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -16,6 +16,22 @@ class TestInterface(unittest.TestCase):
         im = jpeglib.JPEG("examples/IMG_0791.jpeg")
         Y,CbCr,qt = im.read_dct()
         im.close()
+    
+    def test_with_version(self):
+        """Test with statement for version."""
+        # default version
+        jpeglib.version.set('6b')
+        self.assertEqual(jpeglib.version.get(), '6b')
+        # block with new version
+        with jpeglib.version('8d'):
+            self.assertEqual(jpeglib.version.get(), '8d')
+            # block with new version
+            with jpeglib.version('9d'):
+                self.assertEqual(jpeglib.version.get(), '9d')
+            # back to 8d
+            self.assertEqual(jpeglib.version.get(), '8d')
+        # back to 6b
+        self.assertEqual(jpeglib.version.get(), '6b')
             
 
 __all__ = ["TestInterface"]
