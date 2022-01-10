3e35365c7437c0429240cb9695c39623b1809305
Martin Benes <martinbenes1996@gmail.com>
2022-01-10 14:19:07 +0100

    new version with with statement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 # versions
 import os
-__version__ = os.environ.get('VERSION_NEW', '0.7.0')
+__version__ = os.environ.get('VERSION_NEW', '0.7.1')
 libjpeg_versions = {
   '6b': (None,60),
   '8d': (None,80),
