6f6924c6db3568a90069c345fa5cc1cc31b06eb7
Martin Benes <martin.benes@uibk.ac.at>
2022-04-07 14:59:36 +0200

    cstruct docs

--- jpeglib/_cstruct.py
+++ jpeglib/_cstruct.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass,field
 @dataclass
 class CStruct:
     name: str
+    """item name"""
     
     @classmethod
     def name_to_index(cls, name):
@@ -14,23 +15,29 @@ class CStruct:
 
     @property
     def index(self) -> int:
+        """Index getter."""
         return self.name_to_index(self.name)
         
     @property
     def name(self) -> str:
+        """Name getter."""
         return self._name
     @name.setter
     def name(self, name: str):
+        """Name setter."""
         self._name = name
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '<CStruct %s>' % self.name
-    def __str__(self):
+    def __str__(self) -> str:
+        """Converts the class to str, returns name."""
         return self.name
-    def __int__(self):
+    def __int__(self) -> int:
+        """Converts the class to int, returns index."""
         return self.index
     
     @classmethod
     def from_index(cls, index):
+        """Constructs the class from index. Uses the reverse mapping onto name."""
         name = cls.index_to_name(index)
         return cls(name=name)
