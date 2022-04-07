16c2bd0f3df2131c785294e8c8c4d8dae6cbb61d
Martin Benes <martin.benes@uibk.ac.at>
2022-04-07 14:59:36 +0200

    cstruct docs

--- jpeglib/__init__.py
+++ jpeglib/__init__.py
@@ -4,24 +4,27 @@ Python envelope for the popular C library libjpeg for handling JPEG files.
 It offers full control over compression and decompression and exposes DCT coefficients and quantization tables.
 """
 
-import pkg_resources
 # functions
 from .functional import *
+
 # jpeg objects
 from .dct_jpeg import *
 from .spatial_jpeg import *
+
 # cstructs
 from ._colorspace import *
 from ._dctmethod import *
 from ._dithermode import *
 from ._marker import *
-# versions
+
+# libjpeg versions
 from .version import *
 
 # for unit tests
 from ._timer import Timer
 
-# version
+# package version
+import pkg_resources
 try:
     __version__ = pkg_resources.get_distribution("jpeglib").version
 except:
--- jpeglib/_colorspace.py
+++ jpeglib/_colorspace.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+"""Module with color space operations."""
 
 from dataclasses import dataclass,field
 from ._cstruct import CStruct
+
 @dataclass
 class Colorspace(CStruct):
     """Representation of color space.
