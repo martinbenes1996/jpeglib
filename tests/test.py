0993b9af37e7f55b650335f261c19eb46da4958f
nora <nora.hofer@uibk.ac.at>
2022-04-08 11:32:30 +0200

    add progressive unittest

--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,27 @@
 
+from test_dct import *
+from test_interface import *
+from test_shapes import *
+from test_marker import *
+from test_dctmethod import *
+from test_progressive import *
 import sys
 import unittest
+sys.path.append(".")
+
 
 # logging
 if __name__ == "__main__":
     import logging
-    logging.basicConfig(filename="test.log", level = logging.INFO)
+    logging.basicConfig(filename="test.log", level=logging.INFO)
 
-sys.path.append(".")
 
 # === unit tests ===
-from test_dct import *
 # from test_flags import *
-from test_interface import *
-from test_shapes import *
 # from test_spatial import *
 # from test_performance import *
 # from test_version import *
 
-from test_marker import *
-from test_dctmethod import *
 # ==================
 
 
