5fbe216356ab770e06f87a779f6582b9d0bf5ce1
Martin Benes <martin.benes@uibk.ac.at>
2022-04-08 17:03:00 +0200

    release 0.10.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import ctypes
 import setuptools
 import codecs
 import os
-__version__ = os.environ.get('VERSION_NEW', '0.10.1')
+__version__ = os.environ.get('VERSION_NEW', '0.10.2')
 libjpeg_versions = {
     '6b': (None, 60),
     '7': (None, 70),
