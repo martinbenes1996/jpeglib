b5ffbf9f46ed3c8d32123fa25174297c8b6e51d2
Martin Benes <martin.benes@uibk.ac.at>
2022-05-02 16:52:16 +0200

    desperate, renaming jstdhuff.c to jstdhuff.h

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import ctypes
 import setuptools
 import codecs
 import os
-__version__ = os.environ.get('VERSION_NEW', '0.10.6')
+__version__ = os.environ.get('VERSION_NEW', '0.10.7')
 libjpeg_versions = {
     '6b': (None, 60),
     '7': (None, 70),
