cec2d11111afca1df1642f038b17b43805ad2262
Martin Benes <martin.benes@uibk.ac.at>
2022-07-28 12:15:47 +0200

    base abi3 version changed back to 3.8

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ for v in libjpeg_versions:
         ("HAVE_STDLIB_H", 1),
         ("LIBVERSION", libjpeg_versions[v][1]),
         ("HAVE_PROTOTYPES", 1),
-        ("Py_LIMITED_API", "0x03060000"),
+        ("Py_LIMITED_API", "0x03080000"),
     ]
 
     if is_turbo:
