03f40353ebe0cabe6032dac32f57b6e10433a29c
Martin Benes <martin.benes@uibk.ac.at>
2022-09-09 11:27:01 +0200

    added minimal versions

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except ModuleNotFoundError:
     custom_bdist_wheel = {}
 
 # versions
-__version__ = os.environ.get('VERSION_NEW', '0.11.+')
+__version__ = os.environ.get('VERSION_NEW', '0.11.1')
 libjpeg_versions = {
     '6b': (None, 60),
     '7': (None, 70),
