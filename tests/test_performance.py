3659723a977ffc719baa977ae673479aa4f1df66
Martin Benes <martinbenes1996@gmail.com>
2022-10-12 13:39:11 +0200

    compared to 300ms

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -3,7 +3,7 @@
 import logging
 import numpy as np
 from PIL import Image
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, ttest_1samp
 import timeit
 import tempfile
 import unittest
@@ -21,12 +21,7 @@ class TestPerformance(unittest.TestCase):
         del self.tmp
 
     def test_reading(self):
-        # load and time PIL 30 times
-        res_pil = timeit.repeat(
-            lambda: np.array(Image.open("examples/IMG_0791.jpeg")),
-            repeat=50, number=1,
-        )
-        # load and time jpeglib 30 times
+        # load and time jpeglib 50 times
         res_jpeglib = timeit.repeat(
             lambda: jpeglib.read_spatial(
                 "examples/IMG_0791.jpeg",
@@ -34,39 +29,32 @@ class TestPerformance(unittest.TestCase):
             ).spatial,
             repeat=50, number=1,
         )
-        # test in reading, jpeglib is not more than 2x slower than PIL
-        faster_than_pil = ttest_ind(np.array(res_pil)*2, res_jpeglib, alternative='greater')
+        # test in reading, jpeglin is faster than 300ms
+        faster_than_300ms = ttest_1samp(res_jpeglib, .3, alternative='less')
         logging.info(
-            "performance of reading: %.2fs vs. %.2fs of PIL" % (
-                np.mean(res_jpeglib), np.mean(res_pil)
+            "performance of reading: %.2fs" % (
+                np.mean(res_jpeglib)
             )
         )
-        self.assertLess(faster_than_pil.pvalue, .05)
+        self.assertLess(faster_than_300ms.pvalue, .05)
 
     def test_writing(self):
         x = jpeglib.read_spatial("examples/IMG_0791.jpeg").spatial
-        # load and time PIL 30 times
-        pil = []
-        res_pil = timeit.repeat(
-            lambda: Image.fromarray(x).save(self.tmp.name),
-            repeat=50, number=1,
-        )
-        # load and time jpeglib 30 times
+        # load and time jpeglib 50 times
         res_jpeglib = timeit.repeat(
             lambda: jpeglib.from_spatial(x).write_spatial(
                 self.tmp.name,
-                # flags=['DO_FANCY_UPSAMPLING', 'DO_BLOCK_SMOOTHING']
             ),
             repeat=50, number=1,
         )
-        # test in writing, jpeglib is not slower than PIL
-        faster_than_pil = ttest_ind(np.array(res_pil)*2, res_jpeglib, alternative='less')
+        # test in writing, jpeglib is faster than 300ms
+        faster_than_300ms = ttest_1samp(res_jpeglib, .3, alternative='less')
         logging.info(
-            "performance of writing: %.2fs vs. %.2fs of PIL" % (
-                np.mean(res_jpeglib), np.mean(res_pil)
+            "performance of writing: %.2fs" % (
+                np.mean(res_jpeglib)
             )
         )
-        self.assertGreater(faster_than_pil.pvalue, .05)
+        self.assertLess(faster_than_300ms.pvalue, .05)
 
 
 
