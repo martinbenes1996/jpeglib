cd2a4abdee182600f1784a922231bd43d4b060c9
Martin Benes <martin.benes@uibk.ac.at>
2023-02-21 10:31:56 +0100

    ready for release 0.11.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except ModuleNotFoundError:
     custom_bdist_wheel = {}
 
 # versions
-__version__ = os.environ.get('VERSION_NEW', '0.11.5')
+__version__ = os.environ.get('VERSION_NEW', '0.11.6')
 libjpeg_versions = {
     '6b': (None, 60),
     '7': (None, 70),
