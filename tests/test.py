3e5521e3144818fd3d0eb9e639be0e6788ead22a
Martin Benes <martin.benes@uibk.ac.at>
2023-04-19 17:10:12 +0200

    all tests activated again

--- tests/test.py
+++ tests/test.py
@@ -16,15 +16,15 @@ if __name__ == "__main__":
     logging.info(f"{jpeglib.__path__=}")
 
 # === unit tests ===
-# from test_cenum import TestCEnum  # noqa: F401,E402
-# from test_dct import TestDCT  # noqa: F401,E402
-# from test_interface import TestInterface  # noqa: F401,E402
-# from test_marker import TestMarker  # noqa: F401,E402
-# from test_notation import TestNotation  # noqa: F401,E402
-# from test_performance import TestPerformance  # noqa: F401,E402
-# from test_progressive import TestProgressive  # noqa: F401,E402
+from test_cenum import TestCEnum  # noqa: F401,E402
+from test_dct import TestDCT  # noqa: F401,E402
+from test_interface import TestInterface  # noqa: F401,E402
+from test_marker import TestMarker  # noqa: F401,E402
+from test_notation import TestNotation  # noqa: F401,E402
+from test_performance import TestPerformance  # noqa: F401,E402
+from test_progressive import TestProgressive  # noqa: F401,E402
 from test_spatial import TestSpatial  # noqa: F401,E402
-# from test_version import TestVersion  # noqa: F401,E402
+from test_version import TestVersion  # noqa: F401,E402
 
 # from test_flags import TestFlags  # noqa: F401,E402
 # ==================
